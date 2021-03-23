f0f66247b44d1c693c4c21d872627ffa1058cfc7
Noah Wahl <noah.ares@yahoo.de>
2021-03-23 13:35:50 +0100

    Note

--- mt-kahypar/partition/refinement/fm/strategies/gain_cache_strategy.h
+++ mt-kahypar/partition/refinement/fm/strategies/gain_cache_strategy.h
@@ -84,7 +84,7 @@ public:
   MT_KAHYPAR_ATTRIBUTE_ALWAYS_INLINE
   void insertAll(const PHG& phg, const vec<HypernodeID>& nodes) {
     // REVIEW why are you allocating a new vector? just push into the heap vector
-    /* TODO: adjust build_heap for this <22-03-21, @noahares> */
+    /* TODO: adjust build_heap for this, note: heap is protected <22-03-21, @noahares> */
     vec<vec<std::pair<HypernodeID, Gain>>> data(context.partition.k);
     for (auto v : nodes) {
       const PartitionID pv = phg.partID(v);
